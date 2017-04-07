58d2a8ae76eaa03b62036bf8a128a016eb4e5bd2
Adrian-Tudor Panescu <adrian@panescu.com>
2017-04-07 18:06:21 +0300

    Replaced D3 with sigma.js

--- client/src/graph/index.js
+++ client/src/graph/index.js
@@ -1,4 +1,92 @@
-const renderGraph = (raw_nodes) => {
+const renderGraph = (rdf) => {
+  document.getElementById("graphContainer").innerHTML = "";
+  const data = {
+    "nodes": {},
+    "edges": []
+  }
+
+  let node_index = 0;
+  let edges_index = 0;
+
+  for (const node of rdf) {
+    let sindex = -1;
+    let oindex = -1
+
+    // subject
+    if (!data["nodes"].hasOwnProperty(node[0])) {
+      data["nodes"][node[0]] = {
+        "id": node_index,
+        "label": node[0],
+        x: Math.random(),
+        y: Math.random(),
+        "color": "#c7404d",
+        size: 2,
+
+      };
+      sindex = node_index;
+      node_index += 1;
+    } else {
+      sindex = data["nodes"][node[0]]["id"]
+    }
+
+    // object
+    if (!data["nodes"].hasOwnProperty(node[2])) {
+      data["nodes"][node[2]] = {
+        "id": node_index,
+        "label": node[2],
+        x: Math.random(),
+        y: Math.random(),
+        size: 2,
+        "color": "#c7404d"
+      };
+      oindex = node_index;
+      node_index += 1;
+    } else {
+      oindex = data["nodes"][node[2]]["id"]
+    }
+
+    // predicate
+    data["edges"].push({
+      "id": edges_index,
+      "source": sindex,
+      "label": node[1],
+      "target": oindex,
+      "type": "curvedArrow",
+      "color": "#a3cd3d",
+      "count": edges_index
+    });
+    edges_index += 1;
+  }
+  data["nodes"] = Object.values(data["nodes"]);
+  console.log(data);
+
+  const graph = new window.sigma({
+    "graph": data,
+    "container": "graphContainer",
+    defaultNodeColor: '#ec5148',
+    "settings": {
+      "minArrowSize": 8
+    }
+  })
+  const config = {
+    nodeMargin: 3.0,
+    scaleNodes: 1.3
+  };
+
+  //graph.configNoverlap(config);
+  //graph.startNoverlap();
+
+  window.sigma.plugins.dragNodes(graph, graph.renderers[0]);
+  graph.startForceAtlas2();
+  window.setTimeout(() => {
+    graph.killForceAtlas2();
+  }, 3000);
+  window.sigma.plugins.relativeSize();
+
+}
+
+
+const renderGraphD3 = (raw_nodes) => {
 
 
   const nv = window.nv;
---
+++ server/utils/__init__.py
@@ -0,0 +1,1 @@
+oai_root_path = "{http://www.openarchives.org/OAI/2.0/}GetRecord/{http://www.openarchives.org/OAI/2.0/}record/{http://www.openarchives.org/OAI/2.0/}metadata/{http://www.w3.org/1999/02/22-rdf-syntax-ns#}RDF";
