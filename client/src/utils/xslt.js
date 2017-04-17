8d607e6e9611eae7f082bfbfcb6990873bb5956b
Adrian-Tudor Panescu <adrian@panescu.com>
2017-04-17 16:54:13 +0300

    XSLT corrections

--- client/src/utils/xslt.js
+++ client/src/utils/xslt.js
@@ -6,7 +6,7 @@ export default `<?xml version="1.0"?>
   xmlns:ns1="http://purl.org/ontology/bibo/"
   xmlns:ns2="http://purl.org/ontology/bibo/"
   xmlns:ns5="http://www.w3.org/2006/vcard/ns#"
-  exclude-result-prefixes="rdf rdfs ns1">
+  exclude-result-prefixes="rdf rdfs ns1 ns2 ns5">
 
   <xsl:template match="/">
     <oai_dc:dc xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/">
@@ -16,7 +16,7 @@ export default `<?xml version="1.0"?>
           <dc:creator><xsl:value-of select="./@rdf:resource" /></dc:creator>
         </xsl:for-each>
       </xsl:for-each>
-      <xsl:for-each select="rdf:RDF/rdf:Description[@rdf:about='https://figshare.com/articles/Hacking_the_figshare_API_to_Create_Enhanced_Metadata_Records/3219238']">
+      <xsl:for-each select="rdf:RDF/rdf:Description">
         <xsl:for-each select="ns2:freetextKeyword">
           <dc:subject><xsl:value-of select="." /></dc:subject>
         </xsl:for-each>
